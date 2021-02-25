2223649bd38c4cf27ef8aeeb4140993e02adddad
Álvaro Santos <alvrossmiguel@gmail.com>
2021-02-25 11:15:02 -0300

    dia 3

--- src/scripts.js
+++ src/scripts.js
@@ -9,9 +9,13 @@ root.style.setProperty('--game-size', `${GAME_SIZE}px`);
 
 function createBoard(){
 	const boardElement = document.getElementById('board');
+	const elements = [];
 
 	function createElement(options){
 		let { item, top, left } = options;
+
+		const currentElement = { item, currentPosition: { top, left } };
+		elements.push(currentElement);
 
 		const htmlElement = document.createElement('div');
 		htmlElement.className = item;
@@ -20,35 +24,85 @@ function createBoard(){
 
 		boardElement.appendChild(htmlElement);
 
-		function getNewDirection(buttonPressed){
+		function getNewDirection(buttonPressed, position){
 			switch (buttonPressed) {
 				case 'ArrowUp':
-					return { top: top - TILE_SIZE, left: left};
+					return { top: position.top - TILE_SIZE, left: position.left};
 
 			case 'ArrowDown':
-				return { top: top + TILE_SIZE, left: left};
+				return { top: position.top + TILE_SIZE, left: position.left};
 			
 			case 'ArrowLeft':
-					return { top: top, left: left - TILE_SIZE};
+				return { top: position.top, left: position.left - TILE_SIZE};
 					
 			case 'ArrowRight':
-					return { top: top, left: left + TILE_SIZE};
+				return { top: position.top, left: position.left + TILE_SIZE};
 
-			default: return { top: top, left: left };
+			default: 
+				return position;
+			}
+		}
+
+		function validateMovement(position, conflictItem){
+			return (
+				position.left >= 48 &&
+				position.left <= 864 &&
+				position.top >= 96 &&
+				position.top <= 816 &&
+				conflictItem?.item !== "forniture"
+			)
+		}
+
+		function getMovementConflict(position, els){
+			const conflictItem = els.find((currentElement) => {
+				return (
+					currentElement.currentPosition.top === position.top &&
+					currentElement.currentPosition.left === position.left
+				)
+			});
+			return conflictItem;
+		}
+
+		function validateConflict(currentEl, conflictItem){
+			function finishGame(message){
+				setTimeout(() => {
+					alert(message);
+					location.reload();
+				}, 100);
+			}
+
+			if(currentEl.item === 'hero'){
+				if(
+					conflictItem?.item === 'mini-demon' ||
+					conflictItem?.item === 'trap'
+				){
+					finishGame('você morreu');
+				}
+				if(conflictItem?.item === 'chest'){
+					finishGame("você venceu");
+				}
+			}
+			if(currentEl.item === 'mini-demon' && conflictItem?.item === 'hero'){
+				finishGame('vocề morreu');
 			}
 		}
 
 		function move(buttonPressed){
-			console.log('move', buttonPressed);
-			const newDirection = getNewDirection(buttonPressed);
-			top = newDirection.top;
-			left = newDirection.left;
-			htmlElement.style.top = `${newDirection.top}px`;
-			htmlElement.style.left = `${newDirection.left}px`;
+			const newPosition = getNewDirection(buttonPressed, currentElement.currentPosition);
+			const conflictItem = getMovementConflict(newPosition, elements);
+			const isValidMovement = validateMovement(newPosition, conflictItem);
+
+			if(isValidMovement){
+				currentElement.currentPosition = newPosition
+				htmlElement.style.top = `${newPosition.top}px`;
+				htmlElement.style.left = `${newPosition.left}px`;
+
+				validateConflict(currentElement, conflictItem);
+			}
 		}
 
 		return {
-			move: move
+			move
 		};
 	}
 
@@ -88,7 +142,7 @@ function createBoard(){
 	return {
 		createItem,
 		createHero,
-		createEnemy
+		createEnemy,
 	}
 }
 const board = createBoard();
@@ -96,3 +150,8 @@ board.createEnemy({ top: TILE_SIZE*5, left: TILE_SIZE*5 });
 board.createItem({ item: 'trap', top: TILE_SIZE*10, left: TILE_SIZE*10 });
 board.createItem({ item: 'chest', top: TILE_SIZE*15, left: TILE_SIZE*15 });
 board.createHero({ top: TILE_SIZE*16, left: TILE_SIZE*2 });
+
+board.createItem({ item: 'forniture', top: TILE_SIZE*17, left: TILE_SIZE*2 });
+board.createItem({ item: 'forniture', top: TILE_SIZE*2, left: TILE_SIZE*3 });
+board.createItem({ item: 'forniture', top: TILE_SIZE*2, left: TILE_SIZE*8 });
+board.createItem({ item: 'forniture', top: TILE_SIZE*2, left: TILE_SIZE*16 });
